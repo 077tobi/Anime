cb86800bccd33e4752dcddac532a8310d1ba2da2
077tobi <149345589+077tobi@users.noreply.github.com>
2024-08-12 12:27:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const jsoup = require('jsoup'); // Certifique-se de instalar o jsoup: npm install jsoup
+const axios = require('axios'); // Importa a biblioteca axios para requisições HTTP
+const cheerio = require('cheerio'); // Importa a biblioteca cheerio para análise de HTML
 const app = express();
 const port = 3000;
 
@@ -12,22 +12,28 @@ app.get('/episodios/:animeLink', async (req, res) => {
   const animeLink = req.params.animeLink;
 
   try {
-    const html = await jsoup.connect(animeLink).get(); // Faz a requisição ao site
-    const doc = jsoup.parse(html);
-    const itemElements = doc.select("div.div_video_list a.lEp");
+    // Faz a requisição HTTP para o link do anime
+    const response = await axios.get(animeLink);
+    const html = response.data;
+
+    // Analisa o HTML usando cheerio
+    const $ = cheerio.load(html);
+
+    // Seleciona os elementos que representam os links dos episódios
+    const itemElements = $('div.div_video_list a.lEp');
 
     const itemInfoList = [];
-    for (const itemElement of itemElements) {
-      const link = itemElement.attr("href");
-      const titulo = itemElement.text();
+    itemElements.each((index, element) => {
+      const link = $(element).attr('href');
+      const titulo = $(element).text();
 
       itemInfoList.push({
         "link": link,
         "titulo": titulo
       });
-    }
+    });
 
-    res.json(itemInfoList); // Envia a lista de episódios como JSON
+    res.json(itemInfoList);
   } catch (error) {
     console.error("Erro ao obter episódios:", error);
     res.status(500).json({ error: "Erro ao obter episódios" });
