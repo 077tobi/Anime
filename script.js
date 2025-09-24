c0bd3ceb450ea5d8c9f0464dcf24679d14edb83c
077tobi <149345589+077tobi@users.noreply.github.com>
2025-09-24 18:37:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-function trocarEpisodio(novoLink) {
-  const player = document.getElementById("player");
-  player.src = novoLink;
-  player.play();
+async function carregar() {
+  const res = await fetch("https://mundoisekai.online/home/lancamentos");
+  const episodios = await res.json();
+
+  let container = document.getElementById("lista");
+  episodios.forEach(ep => {
+    container.innerHTML += `
+      <div class="anime">
+        <h2>${ep.titulo}</h2>
+        <img src="${ep.capa}" alt="${ep.titulo}">
+        <p>${ep.descricao || ""}</p>
+        <a href="https://mundoisekai.online/anime/detalhes/${ep.id}" target="_blank">Ver Detalhes</a>
+      </div>
+    `;
+  });
 }
+
+carregar();
